59b4a41f81e4619acfecacdf0ee2e9c669a911b0
Grayson White <graysonwhite13@gmail.com>
2022-01-28 10:37:43 -0700

    delete globals.R

--- R/globals.R
+++
@@ -1,5 +0,0 @@
-utils::globalVariables(c(".cl_ds", "arc.check_product", "cond", "count",
-                         "data_tables", "data_uniqueids", "ref_codes",
-                         "savedata_opts", "strata_opts", "table_opts",
-                         "title_opts", "tree", "vcondsppid", "vcondstrid",
-                         "zone_ds"))
