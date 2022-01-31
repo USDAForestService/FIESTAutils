e9a4240338f65073a2b9cd0734c638c0f85665e5
Tracey Frescino <tracey.frescino@usda.gov>
2022-01-31 08:45:27 -0700

    clean up code

--- R/DBinternal.R
+++ R/DBinternal.R
@@ -719,3 +719,82 @@ DBgetbyids <- function(dbconn, ids, layernm, layerid="PLT_CN") {
   DBI::dbClearResult(rs)
   return(dat)
 }
+
+
+#' @rdname internal_desc
+#' @export
+DBcreateSQLite <- function(SQLitefn=NULL, gpkg=FALSE, dbconnopen=FALSE, 
+	outfolder=NULL, outfn.pre=NULL, outfn.date=FALSE, overwrite=FALSE, 
+	returnpath=TRUE, stopifnull=FALSE) {
+  ## DESCRIPTION: 
+  ## Test SQLite connection (SQLite or Geopackage database)
+  ## ARGUMENTS:
+  ## SQLitefn - String. SQLite filename (*.sqlite or *.gpkg)
+  ## dbconnopen - Logical. If TRUE, keep connection to database open
+  ## gpkg	- Logical. If TRUE, geopackage database.
+
+  if (!"DBI" %in% rownames(installed.packages())) {
+    message("accessing SQLite databases requires package DBI")
+  }
+  if (!"RSQLite" %in% rownames(installed.packages())) {
+    message("accessing SQLite databases requires package RSQLite")
+  }
+
+  ## Check gpkg
+  dbext <- ifelse(gpkg, ".gpkg", ".db")
+
+  ## Check filename
+  SQLitePath <- checkfilenm(SQLitefn, outfolder, stopifnull=stopifnull)
+
+  if (is.null(SQLitePath) && is.null(SQLitefn)) {
+    SQLitefn <- "data"
+  }
+
+  if (!is.null(outfn.pre)) {
+    SQLitefn <- paste(outfn.pre, SQLitefn, sep="_")
+  }
+  
+  if (is.na(getext(SQLitefn)) || getext(SQLitefn) == "NA") {
+    SQLitefn <- paste0(SQLitefn, dbext)
+  }
+  if (!dir.exists(dirname(SQLitefn))) {
+    stop("invalid directory path") 
+  }
+  SQLitepath <- getoutfn(SQLitefn, outfn.date=outfn.date, 
+		outfolder=outfolder, overwrite=overwrite, ext="sqlite")
+
+  ## Overwrite file
+  if (file.exists(SQLitepath)) {
+    if (overwrite) {
+      file.remove(SQLitepath) 
+      message("overwriting database... ", SQLitepath)
+    } else {
+      sqlconn <- DBtestSQLite(SQLitefn=SQLitefn, gpkg=gpkg, dbconnopen=dbconnopen,
+		showlist=FALSE)
+      if (dbconnopen) return(sqlconn)    
+    }
+  } else {
+    ## Connect to database
+    message("creating new SQLite database... ")
+    message(SQLitepath)
+    sqlconn <- DBI::dbConnect(RSQLite::SQLite(), SQLitepath, loadable.extensions = TRUE)
+
+    if (dbconnopen) {
+      return(sqlconn)
+    } else {
+      DBI::dbDisconnect(sqlconn)
+      if (returnpath) {
+        return(normalizePath(SQLitepath))
+      } else {
+        return(basename(SQLitepath))
+      }
+    }
+  }
+
+  if (returnpath) {
+    return(SQLitepath)
+  } else {
+    return(basename(SQLitepath))
+  }
+}
+
