042bb19d9d5a115886cafe9209895071b2c73bb4
Tracey Frescino <tracey.frescino@usda.gov>
2023-03-01 17:24:57 -0700

    Update subpsamp.R

--- R/subpsamp.R
+++ R/subpsamp.R
@@ -1,9 +1,13 @@
 #' @rdname internal_desc
 #' @export
 subpsamp <- function(cond, subp_cond, subplot, data_dsn = NULL,
-                     subpuniqueid = "PLT_CN", subpid = "SUBP") {
+                     subpuniqueid = "PLT_CN", subpid = "SUBP", 
+                     ACI = FALSE) {
 
   ## DESCRIPTION: Summarize sampled subplot condition proportions.
+
+  ## Set global variables
+  gui <- FALSE
 
   ## Check cond
   condx <- pcheck.table(cond, tab_dsn=data_dsn, tabnm="cond", 
@@ -17,46 +21,25 @@ subpsamp <- function(cond, subp_cond, subplot, data_dsn = NULL,
   subp_condx <- pcheck.table(subp_cond, tab_dsn=data_dsn, tabnm="subp_cond",
 			caption="Subplot condition table?")
 
-  ## Remove nonsampled conditions by subplotx
+
+
+  ## Remove nonsampled subplots
+  subpwhereqry <- "subp.SUBP_STATUS_CD <> 3"
+  if (ACI) {
+    subpwhereqry <- paste(whereqry, 
+                " and (c.NF_SUBP_STATUS_CD is NULL or NF_SUBP_STATUS_CD != 3)")
+  }
+
+  ## Remove nonsampled conditions
+  cwhereqry <- "c.COND_STATUS_CD <> 5"
+  if (ACI) {
+    cwhereqry <- paste(whereqry, 
+                " and (c.NF_COND_STATUS_CD is NULL or NF_COND_STATUS_CD != 5)")
+  }
+
+  ## Sum condition proportions by subplot
+  #########################################################
   if (!is.null(subplotx)) {
-    if ("SUBP_STATUS_CD" %in% names(subplotx)) {
-      subp.nonsamp.filter <- "SUBP_STATUS_CD != 3"
-      nonsampn <- sum(subplotx[["SUBP_STATUS_CD"]] == 3, na.rm=TRUE)
-      if (length(nonsampn) > 0) {
-        message("removing ", nonsampn, " nonsampled forest conditions")
-      } else {
-        message("assuming all sampled conditions in subplot")
-      }
-    } else {
-      message("assuming all sampled conditions in subplot")
-    }
-      
-#     if (ACI && "NF_COND_STATUS_CD" %in% names(subplotx)) {
-#       subp.nonsamp.filter.ACI <- "(is.na(NF_SUBP_STATUS_CD) | NF_SUBP_STATUS_CD != 3)"
-#       message("removing ", sum(is.na(NF_SUBP_STATUS_CD) & NF_SUBP_STATUS_CD == 3, na.rm=TRUE), 
-# 		" nonsampled nonforest conditions")
-#       if (!is.null(subp.nonsamp.filter)) {
-#         subp.nonsamp.filter <- paste(subp.nonsamp.filter, "&", subp.nonsamp.filter.ACI)
-#       }
-#     }
-    subplotx <- datFilter(x=subplotx, xfilter=subp.nonsamp.filter, 
-		title.filter="subp.nonsamp.filter")$xf
-
-
-    ## Check subpuniqueid and subpid in subplot table
-    subpuniqueid <- pcheck.varchar(var2check=subpuniqueid, varnm="subpuniqueid", 
-		checklst=names(subplotx), caption="Plot ID - subplot", 
-		warn=paste(subpid, "not in tree table"))
-    subpid <- pcheck.varchar(var2check=subpid, varnm="subpid", 
-		checklst=names(subplotx), caption="subplot ID - subplot", 
-		warn=paste(subpid, "not in tree table"))
-    setkeyv(subplotx, c(subpuniqueid, subpid))
- 
-    ## Check subpuniqueid and subpid in subpcond table
-    if (!all(c(subpuniqueid, subpid) %in% names(subp_condx))) {
-      stop("need ", subpuniqueid, " and ", subpid, " variables in subp_cond")
-    }
-    setkeyv(subp_condx, c(subpuniqueid, subpid))
 
     ## Generate query for calculating subplot-level adjustments
     sumcprop.qry <- paste0(
@@ -72,17 +55,11 @@ subpsamp <- function(cond, subp_cond, subplot, data_dsn = NULL,
 			 JOIN subp_condx subc ON (subc.PLT_CN = c.PLT_CN 
 									    and subc.CONDID = c.CONDID
 									    and subc.SUBP = subp.SUBP)
-		  WHERE subp.SUBP_STATUS_CD < 3
-                  AND c.COND_STATUS_CD <> 5 
-             GROUP BY c.PLT_CN, c.CONDID, subc.SUBP, c.COND_STATUS_CD")
+		  WHERE ", subpwhereqry, 
+                  " AND ", cwhereqry, 
+             " GROUP BY c.PLT_CN, c.CONDID, subc.SUBP, c.COND_STATUS_CD")
 
   } else {
-
-    ## Check subpuniqueid and subpid in subpcond table
-    if (!all(c(subpuniqueid, subpid) %in% names(subp_condx))) {
-      stop("need ", subpuniqueid, " and ", subpid, " variables in subp_cond")
-    }
-    setkeyv(subp_condx, c(subpuniqueid, subpid))
 
     ## Generate query for calculating subplot-level adjustments
     sumcprop.qry <- paste0(
@@ -96,8 +73,8 @@ subpsamp <- function(cond, subp_cond, subplot, data_dsn = NULL,
 		  FROM condx c 
 			 JOIN subp_condx subc ON (subc.PLT_CN = c.PLT_CN 
 									    and subc.CONDID = c.CONDID)
-		  WHERE c.COND_STATUS_CD <> 5 
-             GROUP BY c.PLT_CN, c.CONDID, subc.SUBP, c.COND_STATUS_CD")
+		  WHERE ", cwhereqry, 
+             " GROUP BY c.PLT_CN, c.CONDID, subc.SUBP, c.COND_STATUS_CD")
   }
   subpcx <- data.table(sqldf::sqldf(sumcprop.qry))
   return(subpcx)
