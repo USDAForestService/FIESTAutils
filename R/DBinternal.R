c0c3d4a84101e8a6fc8c4ce0211d38651276e083
Tracey Frescino <tracey.frescino@usda.gov>
2023-03-20 14:58:43 -0600

    Remove addftypgrp

--- R/DBinternal.R
+++ R/DBinternal.R
@@ -1021,22 +1021,3 @@ customEvalchk <- function(states, measCur = TRUE, measEndyr = NULL,
 }
 
 
-#' @rdname internal_desc
-#' @export
-addftypgrp <- function(x) {
-  ## DESCRIPTION: appends fortypgrpcd and fortypgrpnm to table
-  ftypnm <- findnm("FORTYPCD", names(x), returnNULL=TRUE)
-  if (is.null(ftypnm)) {
-    stop("FORTYPCD not in x")
-  }
-  ftypgrpnm <- findnm("FORTYPGRPCD", names(x), returnNULL=TRUE)
-  if (!is.null(ftypgrpnm)) {
-    stop("FORTYPGRPCD already in x")
-  }
-  ref_fortypcd <- ref_codes[ref_codes$VARIABLE == "FORTYPCD", c("VALUE", "GROUPCD", "GROUPNM")]
-  names(ref_fortypcd) <- c("FORTYPCD", "FORTYPGRPCD", "FORTYPGRPNM")
-
-  x <- datLUTnm(x, xvar=ftypnm, LUT=ref_fortypcd, LUTvar="FORTYPCD")$xLUT
-  return(x)
-}
-
