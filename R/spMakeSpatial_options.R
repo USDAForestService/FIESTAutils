59b80aa5066f925c69fdb2e3f8caceae0a0bc2f6
Tracey Frescino <tracey.frescino@usda.gov>
2023-05-23 12:35:37 -0600

    update spMakeSpatial_options

--- R/spMakeSpatial_options.R
+++ R/spMakeSpatial_options.R
@@ -10,15 +10,6 @@
 #' @param xy.crs PROJ.4 String or CRS object or Integer EPSG code defining
 #' Coordinate Reference System. (e.g., EPSG:4269-Geodetic coordinate system for
 #' North America, NAD83).
-#' @param prj String. Projection, or coordinate system of the X/Y coordinates
-#' ("longlat", "utm", "aea"). If other, include PROJ.4 string in prj4str.
-#' @param datum String. Datum of projection ("WGS84", "NAD83", "NAD27").
-#' @param zone Integer. If prj="utm", the UTM zone.
-#' @param zoneS Logical. If prj="utm", if the UTM zone is in the Southern
-#' hemisphere.
-#' @param aea.param String. If prj="aea", the associated lat/lon parameters
-#' (USGS: " +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0").  If
-#' other, include PROJ.4 string in prj4str.
 #' @param ... For extendibility.
 #' @return A list of user-supplied parameters and parameter values for strata.
 #' @author Grayson W. White
