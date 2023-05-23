d06b4accf06298322962226625fc6ceb83646d11
Grayson White <graysonwhite13@gmail.com>
2023-05-23 08:18:07 -0400

    import `gdalraster` namespace

--- R/aaa.R
+++ R/aaa.R
@@ -1,4 +1,4 @@
-#' @import data.table sf stats utils 
+#' @import data.table sf stats utils gdalraster
 #' @importFrom methods canCoerce new is
 #' @useDynLib FIESTAutils
 #' @importFrom Rcpp sourceCpp
