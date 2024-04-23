0028ad08bddc48bca1fe46c179873e2e5ffe6fb2
Josh Yamamoto <joshuayamamoto5@gmail.com>
2024-04-23 14:59:38 -0500

    update DBtestPostgreSQL

--- R/DBtestPostgreSQL.R
+++ R/DBtestPostgreSQL.R
@@ -1,25 +1,70 @@
-#' Database - Test a SQLite database table.
+#' Database - Test a PostgreSQL database.
 #' 
-#' Checks a SQLite database.
+#' Checks a PostgreSQL database.
 #' 
+#' @param dbname String. Name of the database on the host.
+#' @param host String. Host name.
+#' @param port String. Port number.
+#' @param user String. User name.
+#' @param password String. Password.
+#' @param dbconnopen Logical. If TRUE, the database connection is returned and not closed.
+#' @param showlist Logical. If TRUE, prints list of tables in database.
+#' @param ... Additional authentication arguments passed to DBI::dbConnect
 #' 
-#' @param dbname String. Name of PostgreSQL database.
-#' @param dbconnopen Logical. If TRUE, the dbconn connection is not closed.
-#' @return An S4 object that inherits from DBIConnection via the DBI package. 
+#' @return An S4 object that inherits from DBIConnection via the DBI package if
+#' dbconnopen = TRUE, or NULL otherwise. 
 #' For more information, see `help(DBI::dbConnect)`. 
 #' @author Tracey S. Frescino
 #' @keywords data
-#' @export DBtestPostgreSQL
-DBtestPostgreSQL <- function(dbname, dbconnopen=TRUE) {
-  ## CONNECT TO ORACLE DATABASE
-  ###########################################
-  if (DBI::dbCanConnect(RPostgreSQL::PostgreSQL(), dbname=dbname)) {
-    message("PostgreSQL connection successful")
-
-    if (dbconnopen) {
-      return(DBI::dbConnect(RPostgreSQL::PostgreSQL(), dbname=dbname))
-    }     
+#' @export 
+DBtestPostgreSQL <- function(dbname = NULL,
+                             host = NULL,
+                             port = NULL,
+                             user = NULL,
+                             password = NULL,
+                             dbconnopen = FALSE,
+                             showlist = TRUE,
+                             ...) {
+  
+  check_conn <- DBI::dbCanConnect(drv = RPostgres::Postgres(),
+                                  dbname = dbname,
+                                  host = host,
+                                  port = port,
+                                  user = user,
+                                  password = password,
+                                  ...)
+  
+  if (check_conn) {
+    message("Connection Successful")
+    pg_conn <- DBI::dbConnect(drv = RPostgres::Postgres(),
+                              dbname = dbname,
+                              host = host,
+                              port = port,
+                              user = user,
+                              password = password,
+                              ...)
+    tablst <- DBI::dbListTables(pg_conn)
   } else {
-    stop("PostgreSQL connection failed")
+    if (!is.null(attr(check_conn, "reason"))) {
+      stop(attr(check_conn, "reason"))
+    } else {
+      stop("PostgreSQL connection failed.")
+    }
   }
+  
+  if (showlist) {
+    message(paste0(tablst, collapse = "\n"))
+  }
+  if (dbconnopen) {
+    out <- pg_conn
+  } else {
+    out <- NULL
+    DBI::dbDisconnect(pg_conn)
+  }
+  
+  return(out)
+  
 }
+
+
+
