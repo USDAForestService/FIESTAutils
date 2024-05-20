db8e5e99f0b6a61c92a8e81c9a63ba6ac9102e97
Grayson White <graysonwhite13@gmail.com>
2024-05-20 16:42:45 -0400

    update

--- R/SABest.pbar.R
+++ R/SABest.pbar.R
@@ -10,63 +10,62 @@ SABest.fit <- function(fmla.dom.unit,
   
   pltdat.unit <- data.frame(pltdat.dom)
   
-  if (SApackage == "spAbundance") {
-    y <- pltdat.unit[[yn]]
-    X <- model.matrix(fmla.dom.unit[-2], pltdat.unit)[,-1]
-    covs <- split(X, col(X))
-    names(covs) <- colnames(X)
+
+  y <- pltdat.unit[[yn]]
+  X <- model.matrix(fmla.dom.unit[-2], pltdat.unit)[,-1]
+  covs <- split(X, col(X))
+  names(covs) <- colnames(X)
+
+  dat.list <- list(
+    y = y,
+    covs = covs
+  )
   
-    dat.list <- list(
-      y = y,
-      covs = covs
-    )
+  if (model.form %in% c("lm", "dvi", "dvc")) {
     
-    if (model.form %in% c("lm", "dvi", "dvc")) {
-      
-      if (model == "lm") {
-        fmla.abund <- fmla.dom.unit[-2]
-      }
-      
-      if (model == "dvi") {
-        # add domain varying intercept term based on dunitvar
-        dvi.term <- reformulate(c(".", paste0("(1 | ", dunitvar, ")")))
-        fmla.abund <- update.formula(fmla.dom.unit[-2], dvi.term)
-      }
-      
-      if (model == "dvc" && length(dvcs) != 0) {
-        # domain varying coefficients and possibly intercepts i.e (tcc | domain)
-        # start by assuming a vector of dvc is passed
-        dvcs <- c("1", dvcs)
-        dvc.term <- reformulate(c(".", paste0("(", dvcs, "|", dunitvar, ")")))
-        fmla.abund <- update.formula(fmla.dom.unit[-2], dvc.term)
-      }
-      
-      mod <- spAbundance::abund(formula = fmla.abund, 
-                                data = dat.list,
-                                family = "Gaussian",
-                                n.batch = 4, 
-                                batch.length = 250,
-                                n.chains = 4, 
-                                n.thin = 20,
-                                n.burn = 500)
-      
-    } else if (model.form %in% c("svi", "svc")) {
-      
-      if (model == "svi") {
-      
-      }
-      
-      if (model == "svc") {
-        
-      }
-      
-      # use svcAbund here
-      # with svc.cols argument
-      
-    } else {
-      stop("")
+    if (model.form == "lm") {
+      fmla.abund <- fmla.dom.unit[-2]
     }
+    
+    if (model.form == "dvi") {
+      # add domain varying intercept term based on dunitvar
+      dvi.term <- reformulate(c(".", paste0("(1 | ", dunitvar, ")")))
+      fmla.abund <- update.formula(fmla.dom.unit[-2], dvi.term)
+    }
+    
+    if (model.form == "dvc" && length(dvcs) != 0) {
+      # domain varying coefficients and possibly intercepts i.e (tcc | domain)
+      # start by assuming a vector of dvc is passed
+      dvcs <- c("1", dvcs)
+      dvc.term <- reformulate(c(".", paste0("(", dvcs, "|", dunitvar, ")")))
+      fmla.abund <- update.formula(fmla.dom.unit[-2], dvc.term)
+    }
+    
+    mod <- spAbundance::abund(formula = fmla.abund, 
+                              data = dat.list,
+                              family = "Gaussian",
+                              n.batch = 4, 
+                              batch.length = 250,
+                              n.chains = 4, 
+                              n.thin = 20,
+                              n.burn = 500)
+    
+  } else if (model.form %in% c("svi", "svc")) {
+    
+    if (model.form == "svi") {
+    
+    }
+    
+    if (model.form == "svc") {
+      
+    }
+    
+    # use svcAbund here
+    # with svc.cols argument
+    
+  } else {
+    stop("")
   }
-  
+
   return(mod)
 }
