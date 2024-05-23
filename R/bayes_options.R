a2e80459bf8708ffc5f8733e7e3347282b83a33b
Grayson White <graysonwhite13@gmail.com>
2024-05-23 11:35:26 -0400

    updates to `bayes_options()`

--- R/bayes_options.R
+++ R/bayes_options.R
@@ -5,8 +5,58 @@
 #' 
 #' If no parameters, an empty list is returned.
 #' 
-#' @param n.batch See spAbundance::abund or spAbundance::svcAbund.
-#' @param batch.length See spAbundance::abund or spAbundance::svcAbund.
+#' @param model.form Model form for Bayesian small area estimation model. 
+#' Currently, accepted options are "lm" for linear model, "dvi" for domain 
+#' varying intercept model, "dvc" for domain varying coefficient(s) model, "svi"
+#' for space varying intercept model, and "svc" for space varying coefficient(s) 
+#' model. "dvi" and "svi" models have a random effect on the intercept, and 
+#' "dvc" and "svc" have a random effect on the intercept, along with any 
+#' selected predictors from `dvcs` or `svcs`.
+#' @param dvcs Character vector. Only used when `model.form` is "dvc". Names of
+#' the columns for which coefficients should vary by domain. Implicitly the 
+#' intercept is included and does not need to specified. 
+#' @param svcs Character vector. Only used when `model.form` is "svc". Names of
+#' the columns for which coefficients should vary in space. Implicitly the
+#' intercept is included and does not need to specified. 
+#' @param coord.names Character vector of length 2. Name of columns for the X
+#' and Y coordinates of plots. Default parameters are tuned for coordinates in 
+#' meters. Used for fitting spatial models. 
+#' @param n.batch Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param batch.length Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param accept.rate Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.omp.threads Additional argument to be passed to spAbundance::abund 
+#' or spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param verbose Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.report Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.burn Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.thin Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.chains Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param save.fitted Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param n.batch Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
+#' @param batch.length Additional argument to be passed to spAbundance::abund or 
+#' spAbundance::svcAbund. See documentation in `spAbundance` package for 
+#' details.
 #' @param ... For extendibility.
 #' @return A list of user-supplied parameters and parameter values for 
 #' fitting Bayesian small area estimation models. 
@@ -21,6 +71,7 @@
 bayes_options <- function(model.form = "dvi",
                           dvcs = NULL,
                           svcs = NULL,
+                          coord.names = c("X", "Y"),
                           n.batch = 4,
                           batch.length = 500,
                           accept.rate = 0.43,
